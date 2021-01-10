f524f41a8d5d8407e599bcc1ce720d6127e25e92
Sean Catangui <seancaat@gmail.com>
2021-01-10 00:19:25 -0500

    adding Activity1 functionality

--- src/views/Activity1.vue
+++ src/views/Activity1.vue
@@ -2,6 +2,19 @@
   <div class="container">
     <Header title="Activity 1"
             description="Guess if the shown image is a wink or a blink"/>
+    <main>
+      <div v-for="(question, index) in quiz.questions" class="question">
+        <p v-show="index === questionIndex">
+          <span>{{ question.p1 }}</span>
+          <span class="blank"></span>
+          <span>{{ question.p2 }}</span>
+        </p>
+      </div>
+    </main> 
+    <div class="button-group">
+      <button @click="next">Wink</button>
+      <button @click="next">Blink</button>
+    </div>   
     <Footer />
   </div>
 </template>
@@ -9,19 +22,58 @@
 <script>
 import Header from "../components/Header.vue";
 import Footer from "../components/Footer.vue";
+import json from "../copy/quiz.json"
 
 export default {
   name: "Activity1",
   metaInfo: {
     title: 'Activity 1'
   },
+  data() {
+    return {
+      quiz: json,
+      questionIndex: 0
+    }
+  },
   components: {
     Header,
     Footer
+  },
+  methods: {
+    next: function() {
+      this.questionIndex++;
+    }
   }
 
 };
 </script>
 
 <style scoped lang="scss">
+  main {
+    margin-top: 15rem;
+    max-width: 60%;
+    margin-left: auto;
+    margin-right: auto;
+
+    .question {
+      p {
+        font-size: 1.2rem;
+        span.blank {
+          border: 1px solid black;
+          box-sizing: border-box;
+          margin: 1em;
+          padding: 0.25em 2.5em;
+        }
+      }
+    }
+  }
+
+  .button-group {
+    text-align: center;
+    margin-top: 5em;
+
+    button {
+      margin-left: 2em;
+    }
+  }
 </style>
