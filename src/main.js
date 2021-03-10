e3af321a171c90d847c4c8182a0c648c6290bfe5
Sean Catangui <seancaat@gmail.com>
2021-03-10 17:10:48 -0500

    update

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueGtag from "vue-gtag";
+
 
 import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueGtag, {
+  config: { id: "G-NX49WMS70T" }
+});
 
 new Vue({
   router,
