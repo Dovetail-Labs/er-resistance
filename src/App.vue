2540bc029388b5c6395176f95731f14433020915
Will Davies <will@canted.co>
2021-04-06 15:35:17 -0700

    self host fonts

--- src/App.vue
+++ src/App.vue
@@ -31,24 +31,6 @@ export default {
 
 <style lang="scss">
 
-/* latin-ext */
-@font-face {
-  font-family: 'Press Start 2P';
-  font-style: normal;
-  font-weight: 400;
-  src: url(https://fonts.gstatic.com/s/pressstart2p/v9/e3t4euO8T-267oIAQAu6jDQyK3nbivNm4I81PZQ.woff2) format('woff2');
-  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
-}
-/* latin */
-@font-face {
-  font-family: 'Press Start 2P';
-  font-style: normal;
-  font-weight: 400;
-  src: url(https://fonts.gstatic.com/s/pressstart2p/v9/e3t4euO8T-267oIAQAu6jDQyK3nVivNm4I81.woff2) format('woff2');
-  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-}
-
-@import url('https://fonts.googleapis.com/css2?family=Cousine:ital,wght@0,400;0,700;1,400;1,700&display=swap');
 
   /* Firefox */
   * {
--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import { VuePlausible } from 'vue-plausible'
 
 import router from './router'
 
+import "@fontsource/press-start-2p/400.css"
+import "@fontsource/cousine/400.css"
+import "@fontsource/cousine/700.css"
+import "@fontsource/cousine/400-italic.css"
+import "@fontsource/cousine/700-italic.css"
+
 Vue.config.productionTip = false;
 
 Vue.use(VuePlausible, {
